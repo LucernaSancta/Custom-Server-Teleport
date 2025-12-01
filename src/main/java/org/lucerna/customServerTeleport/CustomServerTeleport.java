c3336d6cad579d5f781715f235314033ac883c98
LucernaSancta <tommaso.pattaro@hotmail.com>
2025-12-01 23:24:40 +0100

    First test

--- src/main/java/org/lucerna/customServerTeleport/CustomServerTeleport.java
+++ src/main/java/org/lucerna/customServerTeleport/CustomServerTeleport.java
@@ -6,7 +6,14 @@ import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.plugin.Plugin;
 import org.slf4j.Logger;
 
-@Plugin(id = "custom_server_teleport", name = "Custom Server Teleport", version = BuildConstants.VERSION, description = "A simple Velocity plugin to set custom commands and permission to switch server", url = "github.com/LucernaSancta/Custom-Server-Teleport", authors = {"LucernaSancta"})
+@Plugin(
+        id = "custom_server_teleport",
+        name = "Custom Server Teleport",
+        version = BuildConstants.VERSION,
+        description = "A simple Velocity plugin to set custom commands and permission to switch server",
+        url = "github.com/LucernaSancta/Custom-Server-Teleport",
+        authors = {"LucernaSancta"}
+)
 public class CustomServerTeleport {
 
     @Inject
@@ -14,5 +21,6 @@ public class CustomServerTeleport {
 
     @Subscribe
     public void onProxyInitialization(ProxyInitializeEvent event) {
+        logger.info("FUCK PLEASE WORK FUCK PLEASE");
     }
 }
